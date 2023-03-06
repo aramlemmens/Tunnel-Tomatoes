f68493725246de785fab542c7d46fd7eacc173e4
A. Lemmens <aram.lemmensvelden@gmail.com>
2023-03-06 16:07:33 +0100

    [add] Springboot application + hello service

---
+++ src/main/java/com/example/TunnelTomatoes/controller/HelloService.java
@@ -0,0 +1,19 @@
+package com.example.TunnelTomatoes.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+
+public class HelloService {
+    @GetMapping(path = "/")
+    public String hello(){
+        return "hello";
+    }
+    @PostMapping(path = "/process", produces = "text/plain")
+    public String process(@RequestBody String payload) throws Exception {
+        System.out.println(payload);
+        return "200";
+    }
+
+}
---
+++ src/test/java/com/example/TunnelTomatoes/TunnelTomatoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TunnelTomatoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunnelTomatoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
